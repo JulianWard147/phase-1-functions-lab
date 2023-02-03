e91ea793d31f9dc60c9748bb6f11f71d52255e9c
Julian Ward <wardjulianm@gmail.com>
2023-02-03 15:29:30 -0700

    Function lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return(Math.abs(42-someValue))
+}
+
+function distanceFromHqInFeet(someValue){
+    return(distanceFromHqInBlocks(someValue)*264)
+}
+
+function distanceTravelledInFeet(start, destination){
+    return(Math.abs((start-destination)*264))
+}
+
+function calculatesFarePrice(start, destination){
+    let fareDollars
+    let distFeet = distanceTravelledInFeet(start, destination)
+    if (distFeet > 2500)
+        fareDollars = 'cannot travel that far'    
+    
+    else if (distFeet <= 400){
+        fareDollars = 0}
+    
+    else if (distFeet <= 2000){
+        fareDollars = (distFeet-400)*.02}
+
+    else if (distFeet <= 2500){
+        fareDollars = 25} 
+
+    return(fareDollars)
+}
+
